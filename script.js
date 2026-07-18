2b67a4d30fc3c8f779f0a11f9375e97b575d24b9
deevaa-dot <dexusdeevaa@gmail.com>
2026-07-18 21:58:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function showMessage() {
-document.getElementById("intro").style.display = "none";
-document.querySelector("#message").style.display = "flex";
+let secretGift = Math.floor(Math.random()*8) +1;
+function startGame(){
+document.getElementById("intro").style.display="none";
+document.getElementById("game").style.display="block";
 }
+function checkGift(index){
+    const gifts =document.querySelectorAll(".gift");
+    if(index===secretGift){
+        gifts[index].innerHTML="🙀";
+        setTimeout(()=>{
+            document.getElementById("game").style.display="none";
+            document.getElementById("message").style.display="flex";
+            confetti();
+document.getElementById("giftsound").play();
+document.getElementById("birthdaySong").play();
+        },1000);
+    }else{
+        gifts[index].innerHTML="🙅‍♂️";
+        setTimeout(()=>{
+            gifts[index].innerHTML="🎁";
+        },500);
+    }
+}
+
